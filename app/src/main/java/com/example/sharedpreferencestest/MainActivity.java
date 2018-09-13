467bf2cdfc3a13f51902151f92939c20172db994
ooxiaoyan <810978527@qq.com>
2018-09-13 09:34:51 +0800

    Add a button to save data use SharedPreferences

--- app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sharedpreferencestest;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button saveData = findViewById(R.id.save_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.apply();
+            }
+        });
     }
 }
